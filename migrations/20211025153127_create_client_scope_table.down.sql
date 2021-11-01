43d009ad83620a2a3156cdc8949a7ecedca6eeec
Akihiro Suzuki <28677705+akhrszk@users.noreply.github.com>
2021-11-02 02:49:01 +0900

    DBテーブル変更

--- migrations/20211025153127_create_client_scope_table.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS client_scope;
--- migrations/20211025153127_create_client_scope_table.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS client_scope(
-  client_id INT NOT NULL,
-  scope_id INT NOT NULL,
-  PRIMARY KEY (client_id, scope_id)
-);
